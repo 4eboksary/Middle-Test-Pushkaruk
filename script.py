6ec27bf8d43845085ffa5b041acfa3409488cdac
4eboksary <114410308+4eboksary@users.noreply.github.com>
2024-04-05 09:26:04 +0300

    script
    added script, file1, file2, requirements

---
+++ script.py
@@ -0,0 +1,24 @@
+def read_file(filename):
+    with open(filename, 'r') as file:
+        return set(file.readlines())
+
+def write_file(filename, lines):
+    with open(filename, 'w') as file:
+        file.writelines(lines)
+
+def main():
+    file1 = "file1.txt"
+    file2 = "file2.txt"
+    lines1 = read_file(file1)
+    lines2 = read_file(file2)
+
+    same_lines = lines1.intersection(lines2)
+    diff_lines = lines1.symmetric_difference(lines2)
+
+    write_file("same.txt", sorted(same_lines))
+    write_file("diff.txt", sorted(diff_lines))
+
+    print("Результати збережено у файлах 'same.txt' та 'diff.txt'.")
+
+if __name__ == "__main__":
+    main()
